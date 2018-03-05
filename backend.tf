e32179c0e8184ae5111d0e9f2d2e93c4eb8249fc
Lee Saenz <lee.saenz.contractor@magellan.com>
2018-03-05 12:39:21 -0600

    changes to backend config

--- backend.tf
+++ backend.tf
@@ -1,7 +1,12 @@
 terraform {
   backend "s3" {
-    bucket = "terraform_state"
+    bucket = "ml-sre-terraform-state"
     key = "state"
     region = "us-east-1"
+
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    role_arn = "${var.role_arn}"
+
   }
 }
