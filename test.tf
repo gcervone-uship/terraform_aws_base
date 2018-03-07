7f332f994f3ef0e88fcf26522c15db4e303e883a
Lee Saenz <lee.saenz.contractor@magellan.com>
2018-03-07 16:01:14 -0600

    working...

--- test.tf
+++ test.tf
@@ -16,15 +16,24 @@ module "vpc" {
   name = "terraform-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets      = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  database_subnets    = ["10.10.21.0/24", "10.10.22.0/24", "10.10.23.0/24"]
+  elasticache_subnets = ["10.10.31.0/24", "10.10.32.0/24", "10.10.33.0/24"]
+  redshift_subnets    = ["10.10.41.0/24", "10.10.42.0/24", "10.10.43.0/24"]
+
+  create_database_subnet_group = true
 
   enable_nat_gateway = true
   enable_vpn_gateway = false
 
   tags = {
     Terraform = "true"
-    Environment = "prototype"
+    division = "operations"
+    project = "aws base"
+    environment = "proto"
+    envid = "unknown"
+    role = "unknown"
   }
 }
