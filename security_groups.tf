d33079b2f2c2ff995cf9f24f0be369c3393b63af
Lee Saenz <lee.saenz.contractor@magellan.com>
2018-03-21 15:10:54 -0500

    Added more security groups.  Refactored some of the security group code.

--- security_groups.tf
+++ security_groups.tf
@@ -1,138 +1,181 @@
-module "rfc_1918_ssh_security_group" {
+locals {
+
+  common_tags = {
+    Terraform = "true"
+    division = "operations"
+    project = "aws base"
+    environment = "proto"
+    envid = "unknown"
+    role = "unknown"
+  }
+
+  rfc_1918_private_networks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  all_networks = ["0.0.0.0/0"]
+
+  my_vpc = "${module.vpc.vpc_id}"
+
+}
+
+#####################################################
+# Default Security groups open to private networks
+#####################################################
+
+module "private_ssh_security_group" {
   source = "terraform-aws-modules/security-group/aws//modules/ssh"
 
-  name        = "rfc-1918-ssh-sg"
-  description = "Security group with SSH ports open for RFC 1918 private subnets, egress ports are all world open"
-  vpc_id      = "${module.vpc.vpc_id}"
+  name        = "private-ssh-sg"
+  description = "tcp 22 open for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
 
-  // RFC 1918
-  ingress_cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
   auto_ingress_with_self = []
 
-  tags = {
-    Terraform = "true"
-    division = "operations"
-    project = "aws base"
-    environment = "proto"
-    envid = "unknown"
-    role = "unknown"
-  }
-
-
+  tags = "${local.common_tags}"
 }
 
-module "rfc_1918_postgresql_security_group" {
+module "private_http_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "private-http-sg"
+  description = "tcp 80 open for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+module "private_https_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/https-443"
+
+  name        = "private-https-sg"
+  description = "tcp 443 open for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+module "private_rdp_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/rdp"
+
+  name        = "private-rdp-sg"
+  description = "tcp/udp 3389 for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+module "private_redshift_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/redshift"
+
+  name        = "private-redshift-sg"
+  description = "tcp 5439 for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+module "private_mysql_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/mysql"
+
+  name        = "private-mysql-sg"
+  description = "tcp 3306 for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+module "private_postgresql_security_group" {
   source = "terraform-aws-modules/security-group/aws//modules/postgresql"
 
-  name        = "rfc-1918-postgres-sg"
-  description = "Security group with PostgreSQL ports open for RFC 1918 private subnets, egress ports are all world open"
-  vpc_id      = "${module.vpc.vpc_id}"
+  name        = "private-postgres-sg"
+  description = "tcp 5432 open for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
 
-  // RFC 1918
-  ingress_cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
   auto_ingress_with_self = []
 
-  tags = {
-    Terraform = "true"
-    division = "operations"
-    project = "aws base"
-    environment = "proto"
-    envid = "unknown"
-    role = "unknown"
-  }
-
+  tags = "${local.common_tags}"
 }
 
-module "rfc_1918_http_80_security_group" {
+module "private_mssql_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/mssql"
+
+  name        = "private-mssql-sg"
+  description = "tcp 1433/2383/4022 and udp 1434 open for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+module "private_redis_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/redis"
+
+  name        = "private-redis-sg"
+  description = "tcp 6379 for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+module "private_memcached_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/memcached"
+
+  name        = "private-memcached-sg"
+  description = "tcp 11211 for RFC 1918 private subnets, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+
+  ingress_cidr_blocks    = "${local.rfc_1918_private_networks}"
+  auto_ingress_with_self = []
+
+  tags = "${local.common_tags}"
+}
+
+#####################################################
+# Default Security groups open to all networks
+#####################################################
+
+module "public_https_security_group" {
+  source = "terraform-aws-modules/security-group/aws//modules/https-443"
+  
+  name        = "public-https-sg"
+  description = "tcp 443 open to all networks, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
+  
+  ingress_cidr_blocks    = "${local.all_networks}"
+  auto_ingress_with_self = []
+  
+  tags = "${local.common_tags}"
+}
+
+module "public_http_security_group" {
   source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-  name        = "rfc-1918-http-80-sg"
-  description = "Security group with tcp 80 open for RFC 1918 private subnets, egress ports are all world open"
-  vpc_id      = "${module.vpc.vpc_id}"
+  name        = "public-http-sg"
+  description = "tcp 80 open to all networks, egress ports are open to all networks"
+  vpc_id      = "${local.my_vpc}"
 
-  // RFC 1918
-  ingress_cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-
+  ingress_cidr_blocks    = "${local.all_networks}"
   auto_ingress_with_self = []
 
-  tags = {
-    Terraform = "true"
-    division = "operations"
-    project = "aws base"
-    environment = "proto"
-    envid = "unknown"
-    role = "unknown"
-  }
-
-}
-
-module "rfc_1918_https_443_security_group" {
-  source = "terraform-aws-modules/security-group/aws//modules/https-443"
-
-  name        = "rfc-1918-https-443-sg"
-  description = "Security group with tcp 443 open for RFC 1918 private subnets, egress ports are all world open"
-  vpc_id      = "${module.vpc.vpc_id}"
-
-  // RFC 1918
-  ingress_cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
-
-  auto_ingress_with_self = []
-
-  tags = {
-    Terraform = "true"
-    division = "operations"
-    project = "aws base"
-    environment = "proto"
-    envid = "unknown"
-    role = "unknown"
-  }
-
-}
-
-module "world_https_443_security_group" {
-  source = "terraform-aws-modules/security-group/aws//modules/https-443"
-
-  name        = "world-https-443-sg"
-  description = "Security group with tcp 443 open to the world, egress ports are all world open"
-  vpc_id      = "${module.vpc.vpc_id}"
-
-  // Open to the world
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  auto_ingress_with_self = []
-
-  tags = {
-    Terraform = "true"
-    division = "operations"
-    project = "aws base"
-    environment = "proto"
-    envid = "unknown"
-    role = "unknown"
-  }
-
-}
-
-module "world_http_80_security_group" {
-  source = "terraform-aws-modules/security-group/aws//modules/http-80"
-
-  name        = "world-http-80-sg"
-  description = "Security group with tcp 80 open to the world, egress ports are all world open"
-  vpc_id      = "${module.vpc.vpc_id}"
-
-  // Open to the world
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  auto_ingress_with_self = []
-
-  tags = {
-    Terraform = "true"
-    division = "operations"
-    project = "aws base"
-    environment = "proto"
-    envid = "unknown"
-    role = "unknown"
-  }
-
+  tags = "${local.common_tags}"
 }
