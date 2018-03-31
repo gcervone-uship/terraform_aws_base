fa95fb5bf39cf112c6f518d5fc585e8bd82abe37
Lee Saenz <lee.saenz.contractor@magellan.com>
2018-03-30 21:51:43 -0500

    adding output of private subnet

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -3,3 +3,8 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = "${module.vpc.vpc_id}"
 }
+
+output "a_private_subnet" {
+  description = "One of the private subnets for placing a test server."
+  value = "${module.vpc.private_subnets.0.id}"
+}
