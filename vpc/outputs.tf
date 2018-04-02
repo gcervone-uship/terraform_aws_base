9d4be831300251464265cd0c047706ac95d5fd4a
lee.saenz <lee.saenz@macmillan.com>
2018-04-01 23:12:51 -0500

    adding more outputs to vpc

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -4,12 +4,27 @@ output "vpc_id" {
   value       = "${module.vpc.vpc_id}"
 }
 
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value = "${module.vpc.vpc_cidr_block}"
+}
+
+output "public_route_table_ids" {
+  description = "List of IDs of public route tables"
+  value = "${module.vpc.public_route_table_ids}"
+}
+
+output "private_route_table_ids" {
+  description = "List of IDs of private route tables"
+  value = "${module.vpc.private_route_table_ids}"
+}
+
 output "private_subnets" {
-  description = "Private subnets for placing a test server."
+  description = "List of IDs of private subnets"
   value = "${module.vpc.private_subnets}"
 }
 
 output "public_subnets" {
-  description = "Public subnets for placing a test server."
+  description = "List of IDs of public subnets"
   value = "${module.vpc.public_subnets}"
 }
