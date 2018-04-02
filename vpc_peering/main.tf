abd9f2504c9221cd7562810c547f2a4ba6bfaf62
lee.saenz <lee.saenz@macmillan.com>
2018-04-01 23:59:35 -0500

    adding type list for peer vars

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -37,8 +37,8 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
 #
 
 resource "aws_route" "local_to_peer" {
-  count = "${length(var.my_public_route_table_ids)}"  # todo add enable_vpc_peering to check
-
+#   count = "${length(var.my_public_route_table_ids)}"  # todo add enable_vpc_peering to check
+  count = "${var.enable_vpc_peering ? ${length(var.my_public_route_table_ids)} : 0}"
   route_table_id            = "${element(var.my_public_route_table_ids, count.index)}"
   destination_cidr_block    = "${var.peer_vpc_cidr_block}"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
@@ -50,8 +50,9 @@ resource "aws_route" "local_to_peer" {
 #
 
 resource "aws_route" "peer_to_local" {
-  count = "${length(var.peer_public_route_table_ids)}"  # todo add enable_vpc_peering to check
-  
+#  count = "${length(var.peer_public_route_table_ids)}"  # todo add enable_vpc_peering to check
+  count = "${var.enable_vpc_peering ? ${length(var.peer_public_route_table_ids)} : 0}"
+
   provider = "aws.peer"
 
   route_table_id            = "${element(var.peer_public_route_table_ids, count.index)}"
