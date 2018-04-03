40c3916fcf6475a1bba35fb04cdc28bf88a1159f
lee.saenz <lee.saenz@macmillan.com>
2018-04-02 20:35:52 -0500

    Working v0.0.1

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -4,15 +4,13 @@ provider "aws" {
 }
 
 # Used by the aws_vpc_peering_connection
-provider "aws" {
-}
+provider "aws" {}
 
 ##############################################################################
 #                                                                            #
 #                        SETUP VPC PEERING BOTH SIDES                        #
 #                                                                            #
 ##############################################################################
-
 
 # Requester's side of the connection.
 resource "aws_vpc_peering_connection" "peer" {
@@ -21,7 +19,7 @@ resource "aws_vpc_peering_connection" "peer" {
   vpc_id        = "${var.my_vpcid}"
   peer_vpc_id   = "${var.peer_vpcid}"
   peer_owner_id = "${var.peer_vpc_owner_id}"
-  peer_region = "${var.peer_vpc_region}"
+  peer_region   = "${var.peer_vpc_region}"
   auto_accept   = false
 
   tags = "${var.common_tags}"
@@ -38,29 +36,28 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
   tags = "${var.common_tags}"
 }
 
-
 ##############################################################################
 #                                                                            #
 #                          POPULATE ROUTING TABLES                           #
 #                                                                            #
 ##############################################################################
 
-
 locals {
-  my_route_table_ids = "${concat(var.my_private_route_table_ids, var.my_public_route_table_ids)}"
+  my_route_table_ids       = "${concat(var.my_private_route_table_ids, var.my_public_route_table_ids)}"
   my_route_table_ids_count = "${length(local.my_route_table_ids)}"
 
-  peer_route_table_ids = "${concat(var.peer_private_route_table_ids, var.peer_public_route_table_ids)}"
+  peer_route_table_ids       = "${concat(var.peer_private_route_table_ids, var.peer_public_route_table_ids)}"
   peer_route_table_ids_count = "${length(local.peer_route_table_ids)}"
 
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
+  # Workaround below needed because resource may have count=0 -- https://www.terraform.io/upgrade-guides/0-11.html#referencing-attributes-from-resources-with-count-0
+  vpc_peering_connection_id = "${element(concat(aws_vpc_peering_connection.peer.*.id, list("")), 0)}"
 }
 
 #
 # Populate our routing tables with routes to peer
 #
 resource "aws_route" "local_to_peer" {
-  count = "${var.enable_vpc_peering_route_table_updates ? local.my_route_table_ids_count : 0}"
+  count                     = "${var.enable_vpc_peering_route_table_updates ? local.my_route_table_ids_count : 0}"
   route_table_id            = "${element(local.my_route_table_ids, count.index)}"
   destination_cidr_block    = "${var.peer_vpc_cidr_block}"
   vpc_peering_connection_id = "${local.vpc_peering_connection_id}"
