36fd1d19845a30ec5296f5e1343ab2d4a50cacc2
lee.saenz <lee.saenz@macmillan.com>
2018-04-09 13:34:16 -0500

    working...

--- vpc_flowlogs/outputs.tf
+++ vpc_flowlogs/outputs.tf
@@ -1,5 +1,5 @@
 data "aws_region" "current" {}
 
 output "vpc_flowlog_URL" {
-  value = "https://console.aws.amazon.com/cloudwatch/home?region=${data.aws_region.current.id}#logStream:group=${aws_flow_log.vpc_flowlog.log_group_name}"
+  value = "https://console.aws.amazon.com/cloudwatch/home?region=${data.aws_region.current.id}#logStream:group=${aws_flow_log.vpc_flowlog.0.log_group_name}"
 }
