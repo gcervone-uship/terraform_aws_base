54fde209f7769ae7aa2e6b448404fdac258761a0
lee.saenz <lee.saenz@macmillan.com>
2018-04-16 21:15:07 -0500

    working...

--- subdomain/outputs.tf
+++ subdomain/outputs.tf
@@ -1,3 +1,8 @@
 output "zone_id" {
   value = "${aws_route53_zone.subdomain.0.zone_id}"
 }
+
+output "full_subdomain_name" {
+  # concat sub/main then remove trailing dot
+  value = "${replace("${var.subdomain_prefix}.${var.maindomain_name}", "/.$/", "")}"
+}
