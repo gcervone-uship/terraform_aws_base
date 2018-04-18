8445d906619d2127aa227207ef0d160a9206336a
Lee Saenz <lee.saenz.contractor@magellan.com>
2018-04-18 11:52:46 -0500

    removing static count

--- vpc_peering/main.tf
+++ vpc_peering/main.tf
@@ -68,8 +68,7 @@ resource "aws_route" "local_to_peer" {
 # Populate peer's routing tables with routes to us
 #
 resource "aws_route" "peer_to_local" {
-  # count = "${var.enable_vpc_peering_route_table_updates ? local.peer_route_table_ids_count : 0}"
-  count = 0
+  count = "${var.enable_vpc_peering_route_table_updates ? local.peer_route_table_ids_count : 0}"
 
   provider = "aws.peer"
 
